6ac6a43a5c5ac24852f4c4f464daa157fc32ef3a
crossK / naokoO <38545972+rockinruuula1227@users.noreply.github.com>
2022-08-31 07:11:28 +0900

    Create smoothScroll.js

---
+++ smoothScroll.js
@@ -0,0 +1,31 @@
+export const smoothScroll = (event) => {
+  event.preventDefault()
+  pageLinkInnerWithHash(event?.target?.hash ?? "html")
+}
+
+export const smoothScrollWithHash = (hash) => {
+  const el = document.querySelectorAll(hash)[0]
+
+  const targetPosition = el.offsetTop
+  const startPosition = document.scrollingElement.scrollTop
+  let currentPosition = startPosition
+
+  const goDown = currentPosition < targetPosition
+  const interval = setInterval(function() {
+    const mp = movingPosition(startPosition, targetPosition, currentPosition)
+    // mpの返り値が0だと無限ループになるので、最低限移動する値を加減算する
+    currentPosition += goDown ? mp + 5 : - mp - 5
+
+    if (goDown && currentPosition > targetPosition
+      || !goDown && currentPosition < targetPosition) {
+      clearInterval(interval);
+      document.scrollingElement.scrollTop = targetPosition
+    }
+    document.scrollingElement.scrollTop = currentPosition
+  }, 10);
+}
+
+const movingPosition = (start, target, current) => {
+  const sin = Math.sin((current - target) / (start - target) * Math.PI / 2)
+  return sin * 100
+}
